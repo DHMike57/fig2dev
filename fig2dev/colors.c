8738e0477059d9f76e515952d8fd16244bd9a8e4
Thomas Loimer <thomas.loimer@tuwien.ac.at>
2018-05-11 00:35:57 +0200

    Accept blanks in color names

--- fig2dev/colors.c
+++ fig2dev/colors.c
@@ -39,31 +39,45 @@ static int		read_colordb(void);
 static int		numXcolors = 0;
 static struct color_db	*Xcolors;
 
+
+static void
+strip_blank(char *dest, const char *src)
+{
+	while (*src)
+		if (*src == ' ' || *src == '\t')
+			++src;
+		else
+			*dest++ = *src++;
+	*dest = '\0';
+}
+
 int
 lookup_X_color(char *name, RGB *rgb)
 {
 	static bool	have_read_X_colors = false;
-	int		i, n = 0;
-	unsigned short	r, g, b;
+	size_t		len;
 	struct color_db *col;
 
+	len = strlen(name);
 	if (name[0] == '#') {			/* hex color parse it now */
-	    if (strlen(name) == 4) {		/* #rgb */
+	    unsigned short	r, g, b;
+	    int			n = 0;
+	    if (len == 4) {		/* #rgb */
 		    n = sscanf(name,"#%1hx%1hx%1hx",&r,&g,&b);
 		    rgb->red   = ((r << 4) + r) << 8;
 		    rgb->green = ((g << 4) + g) << 8;
 		    rgb->blue  = ((b << 4) + b) << 8;
-	    } else if (strlen(name) == 7) {		/* #rrggbb */
+	    } else if (len == 7) {		/* #rrggbb */
 		    n = sscanf(name,"#%2hx%2hx%2hx",&r,&g,&b);
 		    rgb->red   = r << 8;
 		    rgb->green = g << 8;
 		    rgb->blue  = b << 8;
-	    } else if (strlen(name) == 10) {	/* #rrrgggbbb */
+	    } else if (len == 10) {	/* #rrrgggbbb */
 		    n = sscanf(name,"#%3hx%3hx%3hx",&r,&g,&b);
 		    rgb->red   = r << 4;
 		    rgb->green = g << 4;
 		    rgb->blue  = b << 4;
-	    } else if (strlen(name) == 13) {	/* #rrrrggggbbbb */
+	    } else if (len == 13) {	/* #rrrrggggbbbb */
 		    n = sscanf(name,"#%4hx%4hx%4hx",&r,&g,&b);
 		    rgb->red   = r;
 		    rgb->green = g;
@@ -85,8 +99,13 @@ lookup_X_color(char *name, RGB *rgb)
 	    }
 
 	    /* named color, look in the database we read in */
-	    for (col = Xcolors, i=0; i<numXcolors; ++col, ++i) {
-		if (strcasecmp(col->name, name) == 0) {
+	    for (col = Xcolors; col - Xcolors < numXcolors; ++col) {
+		char	buf[100];
+		char	*strip_name;
+
+		strip_name = len >= 100 ? malloc(len + 1) : buf;
+		strip_blank(strip_name, name);
+		if (strcasecmp(col->name, strip_name) == 0) {
 		    /* found it */
 		    rgb->red = col->red;
 		    rgb->green = col->green;
@@ -107,7 +126,7 @@ read_colordb(void)
 {
     static int		maxcolors = 400;
     FILE		*fp;
-    char		s[100], s1[100], *c1, *c2;
+    char		s[100], s1[100];
     unsigned short	r,g,b;
     struct color_db	*col;
 
@@ -131,6 +150,7 @@ read_colordb(void)
 	    }
 	}
 	if (sscanf(s, "%hu %hu %hu %[^\n]", &r, &g, &b, s1) == 4) {
+	    char	*c1, *c2;
 	    /* remove any white space from the color name */
 	    for (c1=s1, c2=s1; *c2; ++c2) {
 		if (*c2 != ' ' && *c2 != '\t') {
@@ -143,7 +163,7 @@ read_colordb(void)
 	    col->red = r << 8;
 	    col->green = g << 8;
 	    col->blue = b << 8;
-	    col->name = malloc(strlen(s1)+1);
+	    col->name = malloc((size_t)(c1 - s1 + 1));
 	    strcpy(col->name, s1);
 	    ++numXcolors;
 	}
