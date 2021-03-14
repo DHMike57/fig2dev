85eddae630a5afbbeb819a779d82284970fa4ec2
Thomas Loimer <thomas.loimer@tuwien.ac.at>
2021-03-14 19:28:50 +0100

    Correct an error in the call to ghostscript

--- fig2dev/dev/genpdf.c
+++ fig2dev/dev/genpdf.c
@@ -3,7 +3,7 @@
  * Copyright (c) 1991 by Micah Beck
  * Parts Copyright (c) 1985-1988 by Supoj Sutanthavibul
  * Parts Copyright (c) 1989-2015 by Brian V. Smith
- * Parts Copyright (c) 2015-2020 by Thomas Loimer
+ * Parts Copyright (c) 2015-2021 by Thomas Loimer
  *
  * Any party obtaining a copy of these files is granted, free of charge, a
  * full and unrestricted irrevocable, world-wide, paid up, royalty-free,
@@ -47,7 +47,7 @@
  * -o also sets -dBATCH -dNOPAUSE
  */
 #ifdef GSEXE
-#define	GSFMT	GSEXE " -q -dSAFER -sAutoRotatePages=None -sDEVICE=pdfwrite " \
+#define	GSFMT	GSEXE " -q -dSAFER -dAutoRotatePages=/None -sDEVICE=pdfwrite " \
 			"-dPDFSETTINGS=/prepress -o '%s' -"
 #else
 #define GSFMT	""
