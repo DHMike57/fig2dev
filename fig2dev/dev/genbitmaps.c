f42f166cece444361f09433c3d4ab9427d8e0ea3
Thomas Loimer <thomas.loimer@tuwien.ac.at>
2021-06-09 21:22:36 +0200

    Escape first char of transparent color value, #121
    If a transparent color for conversion to gif is given, the first char of
    the color specification is a "#". Since external programs are called via
    popen() to convert the fig2dev output to gif, the "#" must be escaped.

--- fig2dev/dev/genbitmaps.c
+++ fig2dev/dev/genbitmaps.c
@@ -322,7 +322,7 @@ genbitmaps_start(F_compound *objects)
 		} else if (has_ImageMagick()) {
 			if (*gif_transparent)
 				sprintf(fmt, "{ %s%s%s | convert - -transparent"
-						" %s gif:%s; }%s", gscmd,
+						" \\%s gif:%s; }%s", gscmd,
 						antialias, gspipe,
 						gif_transparent, gimend, err);
 			else
@@ -331,7 +331,7 @@ genbitmaps_start(F_compound *objects)
 		} else if (has_GraphicsMagick()) {
 			if (*gif_transparent)
 				sprintf(fmt, "{ %s%s%s | gm convert - "
-						"-transparent %s gif:%s; }%s",
+						"-transparent \\%s gif:%s; }%s",
 						gscmd, antialias, gspipe,
 						gif_transparent, gimend, err);
 			else
